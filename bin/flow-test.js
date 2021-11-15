c88a60160bee8d2d6ff893383e117568431e3e4d
Hiroyasu Nishiyama <hiroyasu.nishiyama.uq@hitachi.com>
2021-11-15 14:04:16 +0900

    merge initial prototype

---
+++ bin/flow-test.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+'use strict'
+
+const nopt = require("nopt");
+const got = require("got");
+
+function parseArgs(args) {
+    const knownOpts = {
+        target: String,
+        list: Boolean,
+        run: String
+    };
+    const shortHands = {
+        't': ["--target"],
+        'l': ["--list"],
+        'r': ["--run"]
+    };
+    return nopt(knownOpts, shortHands, args, 2);
+}
+
+
+async function getList(target) {
+    const url = `${target}/flow-tester/testCase`;
+    try {
+        const res = await got(url);
+        const body = JSON.parse(res.body);
+        return body;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function doList(target) {
+    const list = await getList(target);
+    let id = 0;
+    list.forEach((tcase) => {
+        console.log(`${tcase.name}`);
+        const tests = tcase.tests;
+        tests.forEach((test) => {
+            console.log(`  ${id}: ${test.name}`);
+            id++;
+        });
+    });
+}
+
+async function doRun(target, no) {
+    try {
+        const cases = await getList(target);
+        let id = 0;
+        for (let tcase of cases) {
+            const tests = tcase.tests;
+            for (let test of tests) {
+                if (id === no) {
+                    const url = `${target}/flow-tester/runTestCase/${tcase.id}/${test.id}`;
+                    const res = await got(url);
+                    const body = JSON.parse(res.body);
+                    return body;
+                }
+                id++;
+            }
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return undefined;
+    }
+}
+
+async function main(args) {
+    const opts = parseArgs(args);
+    let target = "http://localhost:1880";
+    if (opts.target) {
+        target = opts.target;
+    }
+    if (opts.list) {
+        await doList(target);
+    }
+    if (opts.run) {
+        const res = await doRun(target, Number(opts.run));
+        console.log(JSON.stringify(res, null, "\t"));
+    }
+}
+
+
+(async () => {
+    main(process.argv);
+})();
--- nodes/flow-test-config.js
+++ nodes/flow-test-config.js
@@ -5,6 +5,8 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, n);
         const node = this;
         node.name = n.name;
+        node.tests = n.tests;
+        node.currentTest = n.currentTest;
     }
     
     RED.nodes.registerType("flow-test-config", FlowTestConfig);
